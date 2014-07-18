3b8309d123e78bedf25a1067005a6c6b3ef41c3f
Chopivy <zbigniew.mlynarski@gmail.com>
2014-07-18 10:02:18 +0200

    Create LM35sensor.ino

---
+++ LM35sensor.ino
@@ -0,0 +1,24 @@
+//LM35 leży jak U
+//Pin 1 of the LM35 goes into +5V of the arduino
+//Pin 2 of the LM35 goes into analog pin A0 of the arduino
+//Pin 3 of the LM35 goes into ground (GND) of the arduino
+//http://goo.gl/Bd60lX
+
+int rawvoltage;
+int tempPin = 0;
+float celsius;
+float millivolts;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  rawvoltage = analogRead(tempPin);
+  millivolts = (rawvoltage/1024.0) * 5000;
+  celsius = millivolts / 10;
+  Serial.print(celsius);
+  Serial.println();
+  delay(10000);
+}
