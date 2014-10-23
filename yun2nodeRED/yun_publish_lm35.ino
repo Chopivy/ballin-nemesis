6438923626cd4b6fb7c42c2d7eeb608989990c8d
Chopivy <zbigniew.mlynarski@gmail.com>
2014-10-23 14:43:31 +0200

    Update yun_publish_lm35.ino

--- yun2nodeRED/yun_publish_lm35.ino
+++ yun2nodeRED/yun_publish_lm35.ino
@@ -56,34 +56,3 @@ void callback(char* topic, byte* payload, unsigned int length) {
 // <f()>: var tempC = parseInt(msg.payload) * (500/1024);
 //        msg.payload = tempC.toString();
 //        return msg;
-
-
-void loop()
-{
-  rawData = analogRead(analogPin);
-  data = String(rawData);
-  len = data.length();
-  
-  for(i=0; i < len; i++)
-    buff[i] = data[i];
-  buff[i] = '\0';
-  
-  if (client.connect("arduinoClient")) {
-    client.publish("yun/sensor/1", buff);
-    Serial.println(data);
-  }
-  delay(2000); //every 2 seconds
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  byte* p = (byte*)malloc(length);
-  memcpy(p,payload,length);
-  client.publish("yun/sensor/1", p, length);
-}
-
-//nodeRed flow:
-// <mqtt| <f()> |debug>
-// <mqtt|: Broker="localhost:1883"; Topic="yun/sensor/1"
-// <f()>: var tempC = parseInt(msg.payload) * (500/1024);
-//        msg.payload = tempC.toString();
-//        return msg;
